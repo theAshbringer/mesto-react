45940c3965178df994a2549efd02cb08b0e98344
theAsbringer <kristya1310@gmail.com>
2022-09-19 17:34:14 +0700

    feat: request and set userInfo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import api from '../utils/api';
 
 const Main = ({ onEditProfile, onAddPlace, onEditAvatar }) => {
+  const [userName, setUserName] = useState('');
+  const [userDescription, setUserDescription] = useState('');
+  const [userAvatar, setUserAvatar] = useState('')
+
+  useEffect(() => {
+    api.loadUserInfo().then(({ name, about, avatar }) => {
+      setUserName(name);
+      setUserDescription(about);
+      setUserAvatar(avatar);
+    });
+  });
 
   return (
     <main className="content">
       <section className="profile">
-        <div className="profile__avatar-container" onClick={onEditAvatar}>
-          <img
-            src="<%=require('./images/avatar.png')%>"
-            alt="Аватар"
-            className="profile__avatar"
-          />
+        <div
+          className="profile__avatar-container"
+          onClick={onEditAvatar}
+          style={{ backgroundImage: `url(${userAvatar})` }}
+        >
         </div>
         <div className="profile__info">
           <div className="profile__name-string">
-            <h1 className="profile__name">Имя</h1>
+            <h1 className="profile__name">{userName}</h1>
             <button
               onClick={onEditProfile}
               className="edit-btn profile__edit-btn"
@@ -23,7 +34,7 @@ const Main = ({ onEditProfile, onAddPlace, onEditAvatar }) => {
               title="Редактировать профиль"
             />
           </div>
-          <p className="profile__description">Описание</p>
+          <p className="profile__description">{userDescription}</p>
         </div>
         <button
           onClick={onAddPlace}
